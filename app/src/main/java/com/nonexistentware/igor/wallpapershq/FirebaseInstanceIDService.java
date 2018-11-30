6cc457ca395cdf76dccbfbcf4555c4e46325f889
Igor <igorsinchuk@mail.ru>
2018-11-30 21:00:33 +0200

    notification function

---
+++ app/src/main/java/com/nonexistentware/igor/wallpapershq/FirebaseInstanceIDService.java
@@ -0,0 +1,21 @@
+package com.nonexistentware.igor.wallpapershq;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class FirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+
+        String refToken = FirebaseInstanceId.getInstance().getToken();
+
+        sendRegToServer(refToken);
+
+        super.onTokenRefresh();
+    }
+
+    private void sendRegToServer(String refToken) {
+
+    }
+}
---
+++ app/src/main/java/com/nonexistentware/igor/wallpapershq/FirebaseMessagingService.java
@@ -0,0 +1,59 @@
+package com.nonexistentware.igor.wallpapershq;
+
+
+
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.RemoteMessage;
+
+public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+    }
+
+    private void showNotification(String title, String body) {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "com.nonexistentware.igor.wallpapershq";
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "Notification",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+
+            notificationChannel.setDescription("Test");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.BLUE);
+            notificationChannel.setVibrationPattern(new long[] {10, 1000, 500, 1000});
+            notificationChannel.enableLights(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.ic_adb)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setContentInfo("Info");
+    }
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+        Log.e("TOKENFIREBASE",s);
+    }
+
+}
